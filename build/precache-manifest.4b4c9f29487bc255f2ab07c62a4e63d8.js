aa31e8a9865cec25290b0b8359e60340f5b7c553
Cristiano <cristiano.goncalves@cubos.io>
2020-05-02 00:16:58 -0300

    feature: add production build.

---
+++ build/precache-manifest.4b4c9f29487bc255f2ab07c62a4e63d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775b5cdf1bc65b02e81079162bf7d3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f3d6ede41a82c248f92",
+    "url": "/static/css/main.94af7ca9.chunk.css"
+  },
+  {
+    "revision": "c041319818d82bb5df07",
+    "url": "/static/js/2.f898f0e2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f898f0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3d6ede41a82c248f92",
+    "url": "/static/js/main.7718fe8a.chunk.js"
+  },
+  {
+    "revision": "220137c34f296e790b99",
+    "url": "/static/js/runtime-main.2dbfb16e.js"
+  }
+]);
