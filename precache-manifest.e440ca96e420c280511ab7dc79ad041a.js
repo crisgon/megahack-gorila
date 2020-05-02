7db93863aff4563bea55cbaa0032c0dc330b6b8f
Cristiano <cristiano.goncalves@cubos.io>
2020-05-02 14:27:04 -0300

    Updates

---
+++ precache-manifest.e440ca96e420c280511ab7dc79ad041a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106c94dee55647919119577885568a16",
+    "url": "/megahack-gorila/index.html"
+  },
+  {
+    "revision": "b84490a3e52939b60d1c",
+    "url": "/megahack-gorila/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "251b3c7a052a0b401221",
+    "url": "/megahack-gorila/static/css/main.09663c69.chunk.css"
+  },
+  {
+    "revision": "b84490a3e52939b60d1c",
+    "url": "/megahack-gorila/static/js/2.64e35f93.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/megahack-gorila/static/js/2.64e35f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251b3c7a052a0b401221",
+    "url": "/megahack-gorila/static/js/main.c54bf5b3.chunk.js"
+  },
+  {
+    "revision": "a0b6059555c1128d1dbf",
+    "url": "/megahack-gorila/static/js/runtime-main.97e40bed.js"
+  }
+]);
