aa53b8f6288b47584cf534974f738997abbdae21
Cristiano <cristiano.goncalves@cubos.io>
2020-05-04 15:57:39 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/megahack-gorila/precache-manifest.f1248a17076164edc0a99c6970ead6d7.js"
+  "/megahack-gorila/precache-manifest.885dc335b4c27d3ef2396052172495ed.js"
 );
 
 self.addEventListener('message', (event) => {
